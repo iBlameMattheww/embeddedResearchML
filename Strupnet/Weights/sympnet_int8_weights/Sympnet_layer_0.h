120598a3f9ec47eb41dd1436cca703fc0a7ff773
iBlameMattheww <matthewobrien2023@gmail.com>
2025-12-29 14:01:11 -0500

    Q16.16 everything now

--- Strupnet/Weights/sympnet_int8_weights/Sympnet_layer_0.h
+++ Strupnet/Weights/sympnet_int8_weights/Sympnet_layer_0.h
@@ -1,15 +1,18 @@
-// SympNet Layer 0 Weights
+// SympNet Layer 0 (Q16.16)
 #ifndef SYMPNET_LAYER_0_H
 #define SYMPNET_LAYER_0_H
 
 #include <stdint.h>
 
 #define COEFFICIENTS_LEN 1
-
 #define WEIGHTS_LEN 2
 
-static const int16_t Sympnet_layer_0_a[COEFFICIENTS_LEN] = {-32767};
+static const int32_t Sympnet_layer_0_a[COEFFICIENTS_LEN] = {
+    -23092
+};
 
-static const int8_t Sympnet_layer_0_w[WEIGHTS_LEN] = {-14, -127};
+static const int32_t Sympnet_layer_0_w[WEIGHTS_LEN] = {
+    -4246, -38946
+};
 
 #endif // SYMPNET_LAYER_0_H
--- Strupnet/Weights/sympnet_int8_weights/Sympnet_layer_1.h
+++ Strupnet/Weights/sympnet_int8_weights/Sympnet_layer_1.h
@@ -1,15 +1,18 @@
-// SympNet Layer 1 Weights
+// SympNet Layer 1 (Q16.16)
 #ifndef SYMPNET_LAYER_1_H
 #define SYMPNET_LAYER_1_H
 
 #include <stdint.h>
 
 #define COEFFICIENTS_LEN 1
-
 #define WEIGHTS_LEN 2
 
-static const int16_t Sympnet_layer_1_a[COEFFICIENTS_LEN] = {-32767};
+static const int32_t Sympnet_layer_1_a[COEFFICIENTS_LEN] = {
+    -23710
+};
 
-static const int8_t Sympnet_layer_1_w[WEIGHTS_LEN] = {-127, 13};
+static const int32_t Sympnet_layer_1_w[WEIGHTS_LEN] = {
+    -38461, 3948
+};
 
 #endif // SYMPNET_LAYER_1_H
