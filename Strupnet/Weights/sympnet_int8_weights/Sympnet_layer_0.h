3845c7d0b9903090e0935158df21c354c6fb0e2c
iBlameMattheww <matthewobrien2023@gmail.com>
2026-01-08 01:14:53 -0500

    housekeeping

--- Strupnet/Weights/sympnet_int8_weights/Sympnet_layer_0.h
+++ Strupnet/Weights/sympnet_int8_weights/Sympnet_layer_0.h
@@ -8,11 +8,11 @@
 #define WEIGHTS_LEN 2
 
 static const int32_t Sympnet_layer_0_a[COEFFICIENTS_LEN] = {
-    -23092
+    -41369
 };
 
 static const int32_t Sympnet_layer_0_w[WEIGHTS_LEN] = {
-    -4246, -38946
+    -40697, 41794
 };
 
 #endif // SYMPNET_LAYER_0_H
--- Strupnet/Weights/sympnet_int8_weights/Sympnet_layer_1.h
+++ Strupnet/Weights/sympnet_int8_weights/Sympnet_layer_1.h
@@ -8,11 +8,11 @@
 #define WEIGHTS_LEN 2
 
 static const int32_t Sympnet_layer_1_a[COEFFICIENTS_LEN] = {
-    -23710
+    -41314
 };
 
 static const int32_t Sympnet_layer_1_w[WEIGHTS_LEN] = {
-    -38461, 3948
+    40804, 41744
 };
 
 #endif // SYMPNET_LAYER_1_H
